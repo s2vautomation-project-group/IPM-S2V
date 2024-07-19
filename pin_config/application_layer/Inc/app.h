697d1abe9ccc51015d83c2253b779d1f710f99a5
Rahul <rahulbo628@gmail.com>
2024-07-19 19:52:23 +0530

    commit1

---
+++ pin_config/application_layer/Inc/app.h
@@ -0,0 +1,16 @@
+/*
+ * app.h
+ *
+ *  Created on: Jun 28, 2024
+ *      Author: user
+ */
+
+#ifndef APPLICATION_INC_APP_H_
+#define APPLICATION_INC_APP_H_
+
+void APPLICATION();
+void extract_data();
+void pin_config();
+
+
+#endif /* APPLICATION_INC_APP_H_ */
