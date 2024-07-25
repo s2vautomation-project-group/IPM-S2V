973a2a9b3e462895d6567deb50ecad1fbf15eaf5
somanathwajantri84@gmail.com <somanathwajantri84@gmail.com>
2024-07-25 20:41:37 +0530

    commit1

---
+++ layers_adc_interrupt/applicationlayer/Inc/ADC.h
@@ -0,0 +1,12 @@
+/*
+ * ADC.h
+ *
+ *  Created on: Jul 23, 2024
+ *      Author: Somanath
+ */
+
+#ifndef INC_ADC_H_
+#define INC_ADC_H_
+void adc_handler(void);
+float Voltage_Calculation(void);
+#endif /* INC_ADC_H_ */
