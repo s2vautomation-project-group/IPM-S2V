671db54f47e7ca7da936bd199c22b836a350d7c4
anand <anandsit119@gmail.com>
2024-07-30 18:32:44 +0530

    ADC, RTC and OLED integration done

---
+++ IPM_Integration/Application_layer/Inc/application_RTC.h
@@ -0,0 +1,9 @@
+#define DS3107_ADDRESS 0xD0
+#include <stdint.h>
+void app(void);
+uint8_t decToBcd(int val);
+
+int bcdToDec(uint8_t val);
+void Set_Time (uint8_t sec, uint8_t min, uint8_t hour, uint8_t dow, uint8_t dom, uint8_t month, uint8_t year);
+void Get_Time (void);
+void Get_Time (void);
---
+++ IPM_Integration/Debug/Common_layer/Src/subdir.mk
@@ -0,0 +1,36 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Common_layer/Src/common_RTC.c \
+../Common_layer/Src/common_adc.c \
+../Common_layer/Src/ssd1306.c \
+../Common_layer/Src/ssd1306_fonts.c 
+
+OBJS += \
+./Common_layer/Src/common_RTC.o \
+./Common_layer/Src/common_adc.o \
+./Common_layer/Src/ssd1306.o \
+./Common_layer/Src/ssd1306_fonts.o 
+
+C_DEPS += \
+./Common_layer/Src/common_RTC.d \
+./Common_layer/Src/common_adc.d \
+./Common_layer/Src/ssd1306.d \
+./Common_layer/Src/ssd1306_fonts.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Common_layer/Src/%.o Common_layer/Src/%.su Common_layer/Src/%.cyclo: ../Common_layer/Src/%.c Common_layer/Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32L432xx -c -I../Core/Inc -I../Drivers/STM32L4xx_HAL_Driver/Inc -I../Drivers/STM32L4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32L4xx/Include -I../Drivers/CMSIS/Include -I"E:/STM IPM project/IPM_Integration/Application_layer/Inc" -I"E:/STM IPM project/IPM_Integration/Common_layer/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Common_layer-2f-Src
+
+clean-Common_layer-2f-Src:
+	-$(RM) ./Common_layer/Src/common_RTC.cyclo ./Common_layer/Src/common_RTC.d ./Common_layer/Src/common_RTC.o ./Common_layer/Src/common_RTC.su ./Common_layer/Src/common_adc.cyclo ./Common_layer/Src/common_adc.d ./Common_layer/Src/common_adc.o ./Common_layer/Src/common_adc.su ./Common_layer/Src/ssd1306.cyclo ./Common_layer/Src/ssd1306.d ./Common_layer/Src/ssd1306.o ./Common_layer/Src/ssd1306.su ./Common_layer/Src/ssd1306_fonts.cyclo ./Common_layer/Src/ssd1306_fonts.d ./Common_layer/Src/ssd1306_fonts.o ./Common_layer/Src/ssd1306_fonts.su
+
+.PHONY: clean-Common_layer-2f-Src
+
