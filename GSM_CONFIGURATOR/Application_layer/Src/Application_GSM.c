75bf3ec475956416eb54f02e6dca7fba326e9c2a
Kavyabharath-R <kavyabharathr.14@gmail.com>
2024-08-30 14:31:56 +0530

    Update Application_GSM.c

--- GSM_CONFIGURATOR/Application_layer/Src/Application_GSM.c
+++ GSM_CONFIGURATOR/Application_layer/Src/Application_GSM.c
@@ -20,69 +20,6 @@ int count=0;
 
 extern UART_HandleTypeDef huart2;
 
-//void DataToGsm(struct data *d)
-//{
-//	uint8_t sec=d->sec;
-//	uint8_t ST=d->scan_time;
-//	//	DataToCloud(d1->time,d1->CH1,d1->CH2,d1->CH3,d1->CH4,d1->D1,d1->D2,d1->D3,d1->D4);
-//	sprintf(buffer_Time,"%02d:%02d:%02d",d->hr,d->min,d->sec);
-//	HAL_UART_Transmit(&huart2, (uint8_t*)d->GPIO, 4, HAL_MAX_DELAY);
-//
-//	if(d->GPIO[0]=="0")
-//	{
-//		strcpy(d->Status1, "High");
-//	}
-//	else
-//	{
-//		strcpy(d->Status1, "Low");
-//	}
-//
-//	if(d->GPIO[1]=="0")
-//	{
-//		strcpy(d->Status2, "High");
-//	}
-//	else
-//	{
-//		strcpy(d->Status2, "Low");
-//	}
-//
-//	if(d->GPIO[2]=="0")
-//	{
-//		strcpy(d->Status3, "High");
-//	}
-//	else
-//	{
-//		strcpy(d->Status3, "Low");
-//	}
-//
-//	if(d->GPIO[3]=="0")
-//	{
-//		strcpy(d->Status4, "High");
-//	}
-//	else
-//	{
-//		strcpy(d->Status4, "Low");
-//	}
-//
-//
-//
-//
-//
-//	if(sec==59)
-//	{
-//		count=count+1;
-//
-//	}
-//
-//
-//	if(count==1)
-//	{
-////		DataToCloud(buffer_Time,d->CH1,d->CH2,d->CH3,d->CH4,d->Status1,d->Status2,d->Status3,d->Status4);
-//		count=0;
-//	}
-//
-//
-//}
 
 
 void DataToGsm()
