62939e6c9cb5bfa3fe08567f878a4860e776bc77
Rahul <rahulbo628@gmail.com>
2024-09-19 14:30:02 +0530

    Integration using F446RE

---
+++ Config_ADC_RTC_GSM(F446RE)/Application/Src/app_GSM.c
@@ -0,0 +1,97 @@
+/*
+ * app_GSM.c
+ *
+ *  Created on: Sep 11, 2024
+ *      Author: KAVYA
+ */
+
+#include "main.h"
+#include "app_GSM.h"
+#include "com_GSM.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "ctype.h"
+#include "data.h"
+char buffer_Time[15];
+char buffer_Date[15];
+	char CH1[10];
+	char CH2[10];
+	char CH3[10];
+	char CH4[10];
+//extern struct data d;
+
+int count=0;
+
+UART_HandleTypeDef huart2;
+
+void DataToGsm(struct data *d)
+{
+	uint8_t sec=d->seconds;
+		//	DataToCloud(d1->time,d1->CH1,d1->CH2,d1->CH3,d1->CH4,d1->D1,d1->D2,d1->D3,d1->D4);
+	sprintf((char*)buffer_Time,"%02d:%02d:%02d",d->hour,d->minutes,d->seconds);
+	sprintf((char*)buffer_Date,"%02d/%02d/%02d",d->dayofmonth,d->month,d->year);
+
+//	HAL_UART_Transmit(&huart2, (uint8_t*)d->GPIO, 4, HAL_MAX_DELAY);
+	sprintf((char*)CH1,"%f",d->adc1_value);
+	sprintf((char*)CH2,"%f",d->adc2_value);
+	sprintf((char*)CH3,"%f",d->adc3_value);
+	sprintf((char*)CH4,"%f",d->adc4_value);
+
+	if(d->GPIO[0]==1)
+	{
+		strcpy(d->Status1, "High");
+	}
+	else
+	{
+		strcpy(d->Status1, "Low");
+	}
+
+	if(d->GPIO[1]==1)
+	{
+		strcpy(d->Status2, "High");
+	}
+	else
+	{
+		strcpy(d->Status2, "Low");
+	}
+
+	if(d->GPIO[2]==1)
+	{
+		strcpy(d->Status3, "High");
+	}
+	else
+	{
+		strcpy(d->Status3, "Low");
+	}
+
+	if(d->GPIO[3]==1)
+	{
+		strcpy(d->Status4, "High");
+	}
+	else
+	{
+		strcpy(d->Status4, "Low");
+	}
+
+
+
+//	DataToCloud(buffer_Time,buffer_Date,d->CH1,d->CH2,d->CH3,d->CH4,d->Status1,d->Status2,d->Status3,d->Status4);
+
+
+	if(sec==59)
+	{
+		count=count+1;
+
+	}
+
+
+	if(count==d->scan_time)
+	{
+		DataToCloud(buffer_Time,buffer_Date,CH1,CH2,CH3,CH4,d->Status1,d->Status2,d->Status3,d->Status4);
+		count=0;
+	}
+
+//	count=0;
+}
+
---
+++ Config_ADC_RTC_GSM(F446RE)/Debug/Application/Src/subdir.mk
@@ -0,0 +1,42 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Application/Src/IPM.c \
+../Application/Src/app_ADC.c \
+../Application/Src/app_GSM.c \
+../Application/Src/app_RTC.c \
+../Application/Src/app_config.c \
+../Application/Src/app_terminal.c 
+
+OBJS += \
+./Application/Src/IPM.o \
+./Application/Src/app_ADC.o \
+./Application/Src/app_GSM.o \
+./Application/Src/app_RTC.o \
+./Application/Src/app_config.o \
+./Application/Src/app_terminal.o 
+
+C_DEPS += \
+./Application/Src/IPM.d \
+./Application/Src/app_ADC.d \
+./Application/Src/app_GSM.d \
+./Application/Src/app_RTC.d \
+./Application/Src/app_config.d \
+./Application/Src/app_terminal.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Application/Src/%.o Application/Src/%.su Application/Src/%.cyclo: ../Application/Src/%.c Application/Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F446xx -c -I../Core/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -I"E:/github 1-f446re/IPM-S2V/IPM-S2V/Integration_with_ADC_RTC_GSM(F446RE)/ADC_RTC_GSM(F446RE)/Application/Inc" -I"E:/github 1-f446re/IPM-S2V/IPM-S2V/Integration_with_ADC_RTC_GSM(F446RE)/ADC_RTC_GSM(F446RE)/Common/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Application-2f-Src
+
+clean-Application-2f-Src:
+	-$(RM) ./Application/Src/IPM.cyclo ./Application/Src/IPM.d ./Application/Src/IPM.o ./Application/Src/IPM.su ./Application/Src/app_ADC.cyclo ./Application/Src/app_ADC.d ./Application/Src/app_ADC.o ./Application/Src/app_ADC.su ./Application/Src/app_GSM.cyclo ./Application/Src/app_GSM.d ./Application/Src/app_GSM.o ./Application/Src/app_GSM.su ./Application/Src/app_RTC.cyclo ./Application/Src/app_RTC.d ./Application/Src/app_RTC.o ./Application/Src/app_RTC.su ./Application/Src/app_config.cyclo ./Application/Src/app_config.d ./Application/Src/app_config.o ./Application/Src/app_config.su ./Application/Src/app_terminal.cyclo ./Application/Src/app_terminal.d ./Application/Src/app_terminal.o ./Application/Src/app_terminal.su
+
+.PHONY: clean-Application-2f-Src
+
